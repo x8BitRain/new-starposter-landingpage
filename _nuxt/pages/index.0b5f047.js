d54cdc97ea8299b46cb9f4e53bb5d3c31672b517
mint <dbell@btr.pm>
2020-08-07 19:41:57 +0200

    26dbac6

---
+++ _nuxt/pages/index.0b5f047.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{168:function(n,t,e){"use strict";e.r(t);var r=e(0).a.extend({data:function(){return{}}}),c=e(26),component=Object(c.a)(r,(function(){var n=this.$createElement;return(this._self._c||n)("div")}),[],!1,null,null,null);t.default=component.exports}}]);
